58f1c668c74081ae2344b8289ec148138ddd68a6
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2021-05-28 19:50:42 -0300

    Add files via upload

--- head.h
+++ head.h
@@ -1,6 +1,6 @@
 /**
  * Created              :   2020.09.02;
- * Last Update          :   2021.04.03;
+ * Last Update          :   2021.05.28;
  * Author               :   Gabriel Marino de Oliveira <ra115114@uem.br>;
  * Supervisor/Advisor   :   Breno Ferraz de Oliveira <>;
  * Notes                :   
@@ -8,18 +8,23 @@
  * Execution            :   time ./simulationRPS.out
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <complex.h>
 #include <gsl/gsl_rng.h>
 
-#define nlog    100     // Number of points which will be printed when used a logarithmic scale;
-#define Ni      500     // Size of the grid in "i";
-#define Nj      500     // Size of the grid in "j";
-#define dt      0.1     // Time interval;
-#define tf      10000   // Total Time, or number of generations;
-#define Pp      0.1     // Predation probability;
-#define Pm      0.8     // Mobility probability - for sthocastic method;
-#define Cm      1.0     // Mobility coeficient  - for RK method;
-#define Pr      0.4     // Reproduction probability;
+#define Ni  500     //  Size of the grid in "i", or abscissa axis;
+#define Nj  500     //  Size of the grid in "j", or ordinate axis;
+#define dt  0.1     //  Time interval;
+#define tf  5000    //  Total Time, or number of generations;
+#define Pp  0.3     //  Predation probability;
+#define Pm  0.4     //  Mobility probability for sthocastic method;
+                    /** Is not necessary to sthocastic method define the Reproduction probability, here we assume which "Pp + Pm + Pr = 1" or "Pr = 1 - (Pp + Pm)",
+                     * and this is why Reproduction probability is only needed to define for RK method**/
+#define Cm  1.0     //  Mobility coeficient for RK method;
+#define Pr  0.4     //  Reproduction probability;
+
+#define np  100                             //  Number of points which will be printed when used a logarithmic scale;
+const double r = pow((tf/dt), (1.0/np));    //  r stands for ratio, will used to generate a arithmetic progression, useful to print equally spaced point
